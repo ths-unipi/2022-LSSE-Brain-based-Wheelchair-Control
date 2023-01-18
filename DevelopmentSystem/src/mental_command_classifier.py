429b7e1914211cda6dfe37b4290f745987ae3692
Matteo Abaterusso <matteoabaterusso@gmail.com>
2023-01-18 12:59:30 +0100

    Fixed Top Five Classifiers evaluation bug

--- DevelopmentSystem/src/mental_command_classifier.py
+++ DevelopmentSystem/src/mental_command_classifier.py
@@ -6,19 +6,19 @@ from sklearn.neural_network import MLPClassifier
 
 class MentalCommandClassifier:
 
-    def __init__(self, uuid: int = 0, training_parameters: dict = None) -> None:
-        self.uuid = uuid
-        if training_parameters is None:
-            self.classifier = None
-        else:
-            self.classifier = MLPClassifier(max_iter=training_parameters['number_of_generations'],
-                                            hidden_layer_sizes=training_parameters['hidden_layer_sizes'])
+    def __init__(self, uuid: int = 0, training_parameters: dict = None, file_name: str = None) -> None:
+        # if file_name isn't given create it
+        if file_name is None:
+            self.uuid = uuid
+            if training_parameters is None:
+                self.classifier = None
+            else:
+                self.classifier = MLPClassifier(max_iter=training_parameters['number_of_generations'],
+                                                hidden_layer_sizes=training_parameters['hidden_layer_sizes'])
 
-    def __int__(self, file_name: str) -> None:
-        self.uuid = int(file_name.split('.')[0])        # get uuid from the file name (es. 1.sav)
-        self.classifier = joblib.load(file_name)
-        # parameters = self.classifier.get_params()     # TODO: valuta se rimuovere
-        # self.training_parameter = TrainingParameter(parameters['max_iter'], parameters['hidden_layer_sizes'])
+        # file_name is given load it from disk
+        else:
+            self.load(file_name)
 
     def train_classifier(self, training_data: DataFrame, training_labels: DataFrame) -> None:
         self.classifier.fit(training_data, training_labels)
@@ -37,7 +37,8 @@ class MentalCommandClassifier:
 
     def load(self, file_name: str) -> None:
         self.uuid = int(file_name.split('.')[0])        # get uuid from the file name (es. 1.sav)
-        self.classifier = joblib.load(file_name)
+        file_path = os.path.join(os.path.abspath('..'), 'data', file_name)
+        self.classifier = joblib.load(file_path)
 
     def store(self) -> None:
         file_path = os.path.join(os.path.abspath('..'), 'data', f'{self.uuid}.sav')
--- DevelopmentSystem/src/top_five_classifiers_evaluator.py
+++ DevelopmentSystem/src/top_five_classifiers_evaluator.py
@@ -19,21 +19,40 @@ class TopFiveClassifierEvaluators:
         validation_error = new_classifier.get_error(self.dataset['validation_data'], self.dataset['validation_labels'])
 
         # add new classifier with validation error in the list, orded by descending validation_error
-        self.top_five_classifiers.append({'classifier': new_classifier, 'validation_error': validation_error})
+        self.top_five_classifiers.append({'uuid': new_classifier.get_uuid(), 'validation_error': validation_error})
         self.top_five_classifiers = sorted(self.top_five_classifiers, key=lambda x: x["validation_error"], reverse=True)
 
-        # the new classifier is one of the top five
-        if len(self.top_five_classifiers) > 5 and self.top_five_classifiers[5]['classifier'] != new_classifier:
-            # save to disk
+        if len(self.top_five_classifiers) > 5:
+
+            # if the new classifier is one of the top five remove from list and disk the sixth
+            if self.top_five_classifiers[5]['uuid'] != new_classifier.get_uuid():
+                # save classifier to disk
+                new_classifier.store()
+
+                # remove the sexth from disk
+                try:
+                    uuid_to_remove = self.top_five_classifiers[5]['uuid']
+                    os.remove(os.path.join(os.path.abspath('..'), 'data', f'{uuid_to_remove}.sav'))
+                except FileNotFoundError:
+                    print('[-] Serialized Classifier not found')
+                    exit(1)
+
+                # remove the sixth from the list
+                self.top_five_classifiers = self.top_five_classifiers[:5]
+
+            # if the new classifier isn't one of the top five remove it from list
+            else:
+                self.top_five_classifiers = self.top_five_classifiers[:5]
+
+        else:
+            # save classifier to disk
             new_classifier.store()
 
-            # remove the sexth from disk
-            uuid_to_remove = self.top_five_classifiers[5].get_uuid()
-            os.remove(os.path.join(os.path.abspath('..'), 'data', f'{uuid_to_remove}.sav'))
-
-            # remove the sixth from the list
-            self.top_five_classifiers = self.top_five_classifiers[:5]
-            print(len(self.top_five_classifiers))
+        # debug
+        deh = []
+        for classifier in self.top_five_classifiers:
+            deh.append(classifier)
+        print(deh)
 
     def get_top_classifiers(self, number_of_generations: int, validation_error_threshold: float) -> dict:
         # structure of the report
@@ -45,12 +64,16 @@ class TopFiveClassifierEvaluators:
 
         # iterate the top five classifier
         for classifier in self.top_five_classifiers:
+            # reload the classifier from disk
+            classifier_name = f'{classifier["uuid"]}.sav'
+            mental_command_classifier = MentalCommandClassifier(file_name=classifier_name)
+
             # insert the classifier in the report
             info_classifier = {
-                'uuid': classifier['classifier'].get_uuid(),
-                'hidden_layer_sizes': classifier['classifier'].get_hidden_layer_sizes(),
-                'training_error': classifier['classifier'].get_error(self.dataset['training_data'],
-                                                                     self.dataset['training_labels']),
+                'uuid': classifier["uuid"],
+                'hidden_layer_sizes': mental_command_classifier.get_hidden_layer_sizes(),
+                'training_error': mental_command_classifier.get_error(self.dataset['training_data'],
+                                                                      self.dataset['training_labels']),
                 'validation_error': classifier['validation_error'],
                 'actual_best': False
             }
