0fb7ee829115ea93368734859e3c12d47091f165
Matteo Abaterusso <matteoabaterusso@gmail.com>
2023-01-18 20:03:27 +0100

    Added Post Operation

--- DevelopmentSystem/test/test_send_dataset.py
+++ DevelopmentSystem/test/test_send_dataset.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     with open(os.path.join(os.path.abspath('.'), 'dataset.json')) as f:
         dataset = json.load(f)
 
-    print(json.dumps(dataset['training'][0], indent=True))
+    post('http://localhost:5000/json', json=dataset)
