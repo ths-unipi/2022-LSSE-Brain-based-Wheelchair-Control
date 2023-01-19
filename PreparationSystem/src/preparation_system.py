3c040bc7e341f24c503c970726939b9bc92c448f
Salvatore Lombardi <salvatore.lombardi29@gmail.com>
2023-01-19 19:19:08 +0100

    Code cleaning

--- PreparationSystem/src/preparation_system.py
+++ PreparationSystem/src/preparation_system.py
@@ -10,7 +10,7 @@ from src.features_extractor import FeaturesExtractor
 class PreparationSystem:
 
     def __init__(self):
-        self._preparation_system_configuration = validate_configuration()
+        self._preparation_system_configuration = self.get_configuration()
         self._raw_session = None
         self._prepared_session = {}
 
@@ -45,39 +45,45 @@ class PreparationSystem:
                 (self._preparation_system_configuration['features'], self._raw_session, self._prepared_session,
                  self._preparation_system_configuration['operative_mode'])
 
-            # print(self._prepared_session)
+            print(self._prepared_session)
 
             # Send prepared session to the endpoint corresponding to the current operating mode
-            # if self._preparation_system_configuration['operative_mode'] == 'development':
-            #     JsonIO.get_instance().send(self._preparation_system_configuration['segregation_endpoint_IP'],
-            #                                self._preparation_system_configuration['segregation_endpoint_port'],
-            #                                self._prepared_session)
-            # elif self._preparation_system_configuration['operative_mode'] == 'execution':
-            #     JsonIO.get_instance().send(self._preparation_system_configuration['execution_endpoint_IP'],
-            #                                self._preparation_system_configuration['execution_endpoint_port'],
-            #                                self._prepared_session)
+            if self._preparation_system_configuration['operative_mode'] == 'development':
+                JsonIO.get_instance().send(self._preparation_system_configuration['segregation_endpoint_IP'],
+                                           self._preparation_system_configuration['segregation_endpoint_port'],
+                                           self._prepared_session)
+            elif self._preparation_system_configuration['operative_mode'] == 'execution':
+                JsonIO.get_instance().send(self._preparation_system_configuration['execution_endpoint_IP'],
+                                           self._preparation_system_configuration['execution_endpoint_port'],
+                                           self._prepared_session)
 
+    @staticmethod
+    def validate_configuration():
+        try:
+            # load configuration from file
+            with open(os.path.join(os.path.abspath('..'), 'PreparationSystemConfiguration.json')) as f:
+                _configuration = json.load(f)
+            # load schema from file
+            with open(os.path.join(os.path.abspath('..'), 'configuration_schema.json')) as f:
+                _configuration_schema = json.load(f)
 
-def validate_configuration():
-    try:
-        # load configuration from file
-        with open(os.path.join(os.path.abspath('..'), 'PreparationSystemConfiguration.json')) as f:
-            _configuration = json.load(f)
-        # load schema from file
-        with open(os.path.join(os.path.abspath('..'), 'configuration_schema.json')) as f:
-            _configuration_schema = json.load(f)
+            # validate configuration schema
+            validate(_configuration, _configuration_schema)
+            return _configuration
 
-        # validate configuration schema
-        validate(_configuration, _configuration_schema)
-        return _configuration
+        except FileNotFoundError:
+            print('Failed to open configuration file')
+            return None
 
-    except FileNotFoundError:
-        print('Failed to open configuration file')
-        exit(1)
+        except ValidationError:
+            print('Config validation failed')
+            return None
 
-    except ValidationError:
-        print('Config validation failed')
-        exit(1)
+    def get_configuration(self):
+        configuration = self.validate_configuration()
+        if configuration is None:
+            exit(1)
+        return configuration
 
 
 if __name__ == '__main__':
